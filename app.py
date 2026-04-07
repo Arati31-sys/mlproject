7e994e040218a34912d484bfa9932964605947e5
Arati31-sys <aratiade31@gmail.com>
2026-04-07 15:26:11 +0530

    Data ingestion

---
+++ app.py
@@ -0,0 +1,22 @@
+from src.mlproject.logger import logging
+from src.mlproject.exception import CustomException
+from src.mlproject.components.data_ingestion import DataIngestion
+from src.mlproject.components.data_ingestion import DataIngestionConfig
+# from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
+# from src.mlproject.components.model_tranier import ModelTrainerConfig,ModelTrainer
+
+import sys
+
+
+if __name__=="__main__":
+    logging.info("The execution has started")
+
+    try:
+        #data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+
+          
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
